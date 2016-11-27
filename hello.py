4a208d92395ace724ee4b39479d4e571fc7a434c
mmtbz <mmtbz@qq.com>
2016-11-28 01:21:30 +0800

    Hello.py which is the main page of our website

--- hello.py
+++ hello.py
@@ -11,13 +11,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
-RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
-             'fox':'http://feeds.foxnews.com/foxnews/latest',
-             'iol':'http://www.iol.co.za/cmlink/1.640',
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+             'fox': 'http://feeds.foxnews.com/foxnews/latest',
+             'iol': 'http://www.iol.co.za/cmlink/1.640',
              'rwanda': 'http://www.inyarwanda.com/articles/index.rss'}
-
-
 
 
 @app.route("/")
@@ -45,6 +42,7 @@ def iol():
 def fox():
     return get_news('fox')
 
+
 body = """<html>
 <body>
 <h1>Headlines </h1>
@@ -55,11 +53,12 @@ body = """<html>
 </body>
 </html> """
 
+
 def get_news(publication):
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
     return u'body'.join((first_article.get("title"), first_article.
-get("published"), first_article.get("summary"))).encode('utf-8')
+                         get("published"), first_article.get("summary"))).encode('utf-8')
 
 
 if __name__ == "__main__":
